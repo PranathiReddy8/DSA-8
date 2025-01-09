c631f71696caf682dfcc2aeaddc44c60537ba218
PranathiReddy8 <2311cs010566@mallareddyuniversity.ac.in>
2025-01-09 18:14:32 +0530

    Loops

---
+++ src/com/mru/methods/MRU.java
@@ -0,0 +1,15 @@
+package com.mru.methods;
+public class MRU{
+	public static void dsa1() {
+	System.out.println("Welcome to DSA1");
+}
+public static void dsa2(String name) {
+	System.out.println(name);
+}
+	public static String dsa3() {
+		return "Welcome to LeetCode";
+	}
+	public static String dsa4(String name) {
+		return "Welcome to "+name;
+	}
+}
