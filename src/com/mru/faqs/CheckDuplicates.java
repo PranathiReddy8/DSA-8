a84ad6bde251b83c663f8f0e75daebe3eb387a9c
PranathiReddy8 <2311cs010566@mallareddyuniversity.ac.in>
2025-01-28 10:47:29 +0530

    Hash

---
+++ src/com/mru/faqs/CheckDuplicates.java
@@ -0,0 +1,22 @@
+package com.mru.faqs;
+import java.util.HashSet;
+
+public class CheckDuplicates {
+
+	public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 4, 1};
+        HashSet<Integer> set = new HashSet<>();
+        boolean flag = false;
+        for (int element : arr) {
+            if (set.contains(element)) {
+            	flag = true;
+                break;
+                
+            } else {
+                set.add(element);
+            }
+        }
+        System.out.println(flag ? "Duplicates are found" : "Duplicates Not Found");
+    }
+}
+
